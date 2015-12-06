323a15946200ca8b43aac928edbe099057a13b5f
Ben.LI <20796698@qq.com>
2015-12-06 10:30:09 +0800

    初始化

--- bin-anthingtest/src/main/java/com/cheuks/bin/net/nio/server_v1.java
+++
@@ -1,5 +0,0 @@
-package com.cheuks.bin.net.nio;
-
-public class server_v1 {
-
-}
---
+++ bin-anythingtest/src/main/java/org/bin/anythingtest/App.java
@@ -0,0 +1,13 @@
+package org.bin.anythingtest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
