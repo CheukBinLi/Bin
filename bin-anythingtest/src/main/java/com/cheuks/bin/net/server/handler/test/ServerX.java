38a3c888231fb200178cce3bb6e5ba68511fad62
Ben.LI <20796698@qq.com>
2016-01-07 23:22:35 +0800

    a

--- bin-anythingtest/src/main/java/com/cheuks/bin/net/server/handler/test/ServerX.java
+++ bin-anythingtest/src/main/java/com/cheuks/bin/net/server/handler/test/ServerX.java
@@ -2,6 +2,7 @@ package com.cheuks.bin.net.server.handler.test;
 
 import com.cheuks.bin.net.server.NioServer;
 import com.cheuks.bin.net.server.Server;
+import com.cheuks.bin.net.server.event.EventInfo;
 
 public class ServerX {
 	public static void main(String[] args) throws Throwable {
@@ -9,6 +10,7 @@ public class ServerX {
 		server.start(2000, 20000);
 		server.addService(10088, Server.SERVICE_TYPE_RMI).addService(10087, Server.SERVICE_TYPE_MESSAGE);
 		server.addHandler(new ServiceHandlerTest());
+		server.addEventHandle(new EventInfo(new LogWriteEvent(), new LogHandleEvent()), 16);
 		//		server.addEventHandle(new EventInfo(new RmiWriteEvent(), new RmiHandleEvent()), Server.SERVICE_TYPE_RMI);
 		//		server.addEventHandle(new EventInfo(new MessageWriteEvent(), new MessageHandleEvent()), Server.SERVICE_TYPE_MESSAGE);
 	}
