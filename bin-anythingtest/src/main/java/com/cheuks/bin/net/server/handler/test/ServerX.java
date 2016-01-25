f2208703f0dd9bf4394d33881c3507d103896ac0
Zhuo.Ben.Li <20796698@qq.com>
2016-01-25 10:38:39 +0800

    b

--- bin-anythingtest/src/main/java/com/cheuks/bin/net/server/handler/test/ServerX.java
+++ bin-anythingtest/src/main/java/com/cheuks/bin/net/server/handler/test/ServerX.java
@@ -2,7 +2,6 @@ package com.cheuks.bin.net.server.handler.test;
 
 import com.cheuks.bin.net.server.NioServer;
 import com.cheuks.bin.net.server.Server;
-import com.cheuks.bin.net.server.event.EventInfo;
 import com.cheuks.bin.net.util.DefaultSerializImpl;
 
 public class ServerX {
@@ -13,7 +12,7 @@ public class ServerX {
 		server.setSerializ(DefaultSerializImpl.class);
 		server.addService(10088, Server.SERVICE_TYPE_RMI).addService(10087, Server.SERVICE_TYPE_MESSAGE);
 		server.addHandler(new ServiceHandlerTest());
-		server.addEventHandle(new EventInfo(new LogWriteEvent(), new LogHandleEvent()), 16);
+//		server.addEventHandle(new EventInfo(new LogWriteEvent(), new LogHandleEvent()), 16);
 		//		server.addEventHandle(new EventInfo(new RmiWriteEvent(), new RmiHandleEvent()), Server.SERVICE_TYPE_RMI);
 		//		server.addEventHandle(new EventInfo(new MessageWriteEvent(), new MessageHandleEvent()), Server.SERVICE_TYPE_MESSAGE);
 	}
