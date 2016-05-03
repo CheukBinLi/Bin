1cb24399a75186a9de88ace0b22ebafcff041c71
Ben <20796698@qq.com>
2016-05-03 18:32:38 +0800

    aaa

---
+++ bin-anythingtest/src/main/java/com/cheuks/bin/anythingtest/freemark_test/CreateFile.java
@@ -0,0 +1,21 @@
+package com.cheuks.bin.anythingtest.freemark_test;
+
+import freemarker.cache.FileTemplateLoader;
+import freemarker.template.Configuration;
+
+public class CreateFile {
+
+	public void create(Class<?> c) {
+
+		// dao
+		// daoImpl
+		// service
+		// service
+		// controller
+
+		Configuration config = new Configuration(Configuration.VERSION_2_3_0);
+//		FileTemplateLoader fileTemplateLoader = new FileTemplateLoader();
+
+	}
+
+}
