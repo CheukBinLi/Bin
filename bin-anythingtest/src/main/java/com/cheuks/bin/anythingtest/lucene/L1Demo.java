2869067c6f16d64395977e835abc1237c7f117b6
Ben <20796698@qq.com>
2016-06-17 18:05:52 +0800

    x

--- bin-anythingtest/src/main/java/com/cheuks/bin/anythingtest/lucene/L1Demo.java
+++ bin-anythingtest/src/main/java/com/cheuks/bin/anythingtest/lucene/L1Demo.java
@@ -1,5 +1,6 @@
 package com.cheuks.bin.anythingtest.lucene;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.FileVisitResult;
@@ -11,9 +12,12 @@ import java.util.Collection;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 
+import org.apache.lucene.analysis.core.SimpleAnalyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
 import org.apache.lucene.document.Field.Store;
+import org.apache.lucene.document.FieldType;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.DirectoryReader;
@@ -28,12 +32,19 @@ import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.search.TopFieldDocs;
+import org.apache.lucene.search.highlight.Highlighter;
+import org.apache.lucene.search.highlight.InvalidTokenOffsetsException;
+import org.apache.lucene.search.highlight.QueryScorer;
+import org.apache.lucene.search.highlight.SimpleFragmenter;
+import org.apache.lucene.search.highlight.SimpleHTMLFormatter;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.store.IOContext;
 import org.apache.lucene.store.IndexInput;
 import org.apache.lucene.store.IndexOutput;
 import org.apache.lucene.store.Lock;
+
+import freemarker.template.SimpleScalar;
 
 public class L1Demo {
 
@@ -60,6 +71,7 @@ public class L1Demo {
 		// doc.add(new TextField("content", Files.newBufferedReader(Paths.get(indexDocument + "/1.txt"), Charset.forName("utf-8"))));
 		// writer.addDocument(doc);
 		// 添加文件索引
+		writer.deleteAll();
 		docs(writer, Paths.get(indexDocument));
 		// 完成提交
 		writer.commit();
@@ -68,15 +80,6 @@ public class L1Demo {
 
 	public void docs(final IndexWriter indexWriter, final Path path) throws IOException {
 		if (Files.isDirectory(path)) {
-			// Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
-			// @Override
-			// public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
-			// System.out.println(file.getFileName().toString());
-			// docs(indexWriter, file);
-			// return FileVisitResult.CONTINUE;
-			// }
-			//
-			// });
 			Files.walkFileTree(path, new FileVisitor<Path>() {
 
 				public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
@@ -106,14 +109,18 @@ public class L1Demo {
 		Document doc = null;
 
 		doc = new Document();
-		doc.add(new StringField("path", indexDocument, Store.YES));
-
-		// Files.newInputStream(pa, options)
-		doc.add(new TextField("content", Files.newBufferedReader(path)));
+		doc.add(new StringField("path", path.toString(), Store.YES));
+		// doc.add(new TextField("content", Files.newBufferedReader(path)));
+		String line = null;
+		StringBuffer sb = new StringBuffer();
+		BufferedReader br = Files.newBufferedReader(path);
+		while (null != (line = br.readLine()))
+			sb.append(line).append("\n");
+		doc.add(new TextField("content", sb.toString(), Store.YES));
 		indexWriter.addDocument(doc);
 	}
 
-	public void scarch(String str) throws IOException, ParseException {
+	public void scarch(String str) throws IOException, ParseException, InvalidTokenOffsetsException {
 
 		// IndexSearcher indexSearcher = new IndexSearcher(DirectoryReader.open(FSDirectory.open(Paths.get(indexSearch))));
 		IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(indexSearch)));
@@ -121,16 +128,27 @@ public class L1Demo {
 		QueryParser parser = new QueryParser("content", new StandardAnalyzer());
 		Query query = parser.parse(str);
 		TopDocs topDocs = indexSearcher.search(query, 10000);
+
+		SimpleHTMLFormatter htmlFormatter = new SimpleHTMLFormatter("<span style=\"color:red\">", "</span>");
+		Highlighter highlighter = new Highlighter(htmlFormatter, new QueryScorer(query));
+
+		int n = 0;
+		String content;
 		for (ScoreDoc doc : topDocs.scoreDocs) {
-			int n = doc.doc;
+			n = doc.doc;
 			Document document = indexSearcher.doc(n);
+			content = document.get("content");
 			System.err.println(document.get("path"));
-			System.err.println(document.get("content"));
+			highlighter.setTextFragmenter(new SimpleFragmenter(content.length()));
+
+			System.err.println(highlighter.getBestFragment(new SimpleAnalyzer(), "content", content));
+
+			// System.err.println(document.get("content"));
 		}
 	}
 
-	public static void main(String[] args) throws IOException, ParseException {
-		// new L1Demo().createSearch();
+	public static void main(String[] args) throws IOException, ParseException, InvalidTokenOffsetsException {
+		new L1Demo().createSearch();
 		new L1Demo().scarch("import");
 	}
 
---
+++ bin-db-manager/src/main/java/com/cheuks/bin/db/manager/dao/AbstractDao.java
@@ -0,0 +1,79 @@
+package com.cheuks.bin.db.manager.dao;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+
+import com.cheuks.bin.db.manager.DBAdapter;
+
+public abstract class AbstractDao<entity, ID extends Serializable> implements BaseDao<entity, ID> {
+
+	public abstract Class<entity> getEntityClass();
+
+	public abstract DBAdapter getDBAdapter();
+
+	public entity get(ID id) throws Throwable {
+		return getDBAdapter().get(getEntityClass(), id);
+	}
+
+	public List<entity> getList(int page, int size) throws Throwable {
+		return getDBAdapter().getList(getEntityClass(), page, size);
+	}
+
+	public List<entity> getList(Map<String, Object> params, int page, int size) throws Throwable {
+		return getDBAdapter().getListByXqlQueryName(getDBAdapter().queryNameFormat(getEntityClass(), "list"), true, true, params, page, size);
+	}
+
+	public <T> List<T> getList(String queryName, Map<String, Object> params, boolean isFromat, int page, int size) throws Throwable {
+		return getDBAdapter().getListByXqlQueryName(getDBAdapter().queryNameFormat(getEntityClass(), queryName), true, isFromat, params, page, size);
+		// return getDBAdapter().getListByXqlQueryName(getDBAdapter().queryNameFormat(getEntityClass(), queryName), true, isFromat, params, page, size);
+	}
+
+	public <T> List<T> getListCustomQueryName(String queryName, Map<String, Object> params, boolean isFromat, int page, int size) throws Throwable {
+		// return getDBAdapter().getListByXqlQueryName(queryName.toLowerCase(), true, isFromat, params, page, size);
+		return getDBAdapter().getListByXqlQueryName(queryName.toLowerCase(), true, isFromat, params, page, size);
+	}
+
+	public List<entity> getListEntity(String queryName, Map<String, Object> params, boolean isFromat, int page, int size) throws Throwable {
+		return getDBAdapter().getListByXqlQueryName(getDBAdapter().queryNameFormat(getEntityClass(), queryName), true, isFromat, params, page, size);
+		// return getDBAdapter().getListByXqlQueryName(getDBAdapter().queryNameFormat(getEntityClass(), queryName), true, isFromat, params, page, size);
+	}
+
+	public List<entity> getListEntityCustomQueryName(String queryName, Map<String, Object> params, boolean isFromat, int page, int size) throws Throwable {
+		// return getDBAdapter().getListByXqlQueryName(queryName.toLowerCase(), true, isFromat, params, page, size);
+		return getDBAdapter().getListByXqlQueryName(queryName.toLowerCase(), true, isFromat, params, page, size);
+	}
+
+	public entity save(entity o) throws Throwable {
+		return getDBAdapter().save(o);
+	}
+
+	public void saveOrUpeate(entity o) throws Throwable {
+		getDBAdapter().saveOrUpdate(o);
+	}
+
+	public void update(entity o) throws Throwable {
+		getDBAdapter().update(o);
+	}
+
+	public void delete(entity o) throws Throwable {
+		getDBAdapter().delete(o);
+	}
+
+	public Object uniqueResult(String queryName, boolean isFormat, Map<String, Object> params) throws Throwable {
+		return getDBAdapter().uniqueResult(getDBAdapter().queryNameFormat(getEntityClass(), queryName), true, isFormat, params);
+	}
+
+	public Object uniqueResultCustomQueryName(String queryName, boolean isFormat, Map<String, Object> params) throws Throwable {
+		return getDBAdapter().uniqueResult(queryName.toLowerCase(), true, isFormat, params);
+	}
+
+	public int executeUpdate(String queryName, Map<String, Object> params, boolean isHql, boolean isFromat) throws Throwable {
+		return getDBAdapter().executeUpdate(getDBAdapter().queryNameFormat(getEntityClass(), queryName), params, isHql, isFromat);
+	}
+
+	public int executeUpdateCustomQueryName(String queryName, Map<String, Object> params, boolean isHql, boolean isFromat) throws Throwable {
+		return getDBAdapter().executeUpdate(queryName.toLowerCase(), params, isHql, isFromat);
+	}
+
+}
