3e045a8b757f81a9899970d329ccf6342f00eed8
BenPack <20796698@qq.com>
2016-06-20 20:41:12 +0800

    a

--- bin-webtest/src/main/java/Controller/shiro/cache/AbstractRedisCacheManager.java
+++ bin-webtest/src/main/java/Controller/shiro/cache/AbstractRedisCacheManager.java
@@ -1,55 +1,16 @@
 package Controller.shiro.cache;
 
-import java.io.Serializable;
-
 import redis.clients.jedis.JedisCommands;
 
-public abstract class AbstractRedisCacheManager<J extends JedisCommands> implements CacheManager<String, Serializable> {
+public abstract class AbstractRedisCacheManager<J extends JedisCommands, V> implements CacheManager<String, V> {
 
-	Serialize serialize;
-
-	J tempJedis;
-
-	public void update(Cache<String, Serializable> cache) throws Throwable {
-		(tempJedis = getResource()).set(cache.getKey(), new String(serialize.encode(cache.getValue())));
-		destory(tempJedis);
-	}
-
-	public void delete(String k) throws Throwable {
-		(tempJedis = getResource()).del(k);
-		destory(tempJedis);
-	}
-
-	public void create(Cache<String, Serializable> cache) throws Throwable {
-		update(cache);
-	}
-
-	public Serializable getAndSet(Cache<String, Serializable> cache) throws Throwable {
-		try {
-			return (tempJedis = getResource()).getSet(cache.getKey(), new String(serialize.encode(cache.getValue())));
-		} finally {
-			destory(tempJedis);
-		}
-	}
-
-	public Serializable get(String k) throws Throwable {
-		try {
-			return (tempJedis = getResource()).get(k);
-		} finally {
-			destory(tempJedis);
-		}
-	}
+	private Serialize serialize;
 
 	abstract J getResource();
 
 	abstract void init();
 
 	abstract void destory(J jedis);
-
-	public AbstractRedisCacheManager() {
-		super();
-		init();
-	}
 
 	public Serialize getSerialize() {
 		return serialize;
