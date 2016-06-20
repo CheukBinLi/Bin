6bbcaed00b3da1be68b422b853555cb41f3ce837
BenPack <20796698@qq.com>
2016-06-21 00:26:20 +0800

    a

---
+++ bin-webtest/src/main/java/Controller/custom/MyAnnotation.java
@@ -0,0 +1,14 @@
+package Controller.custom;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyAnnotation {
+	String value() default "wahaha";
+}
