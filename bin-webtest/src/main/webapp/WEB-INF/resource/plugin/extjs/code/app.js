a1d74c9e51876e1cc02e69cd95c6a6c000e9a16f
BenPack <20796698@qq.com>
2016-07-13 23:08:11 +0800

    ext

---
+++ bin-webtest/src/main/webapp/WEB-INF/resource/plugin/extjs/code/app.js
@@ -0,0 +1,24 @@
+Ext.onReady(function() {
+	Ext.QuickTips.init();
+	Ext.Loader.setConfig({
+				enabled : true
+			});
+	Ext.application({
+				// requires : ['Ext.container.Viewport'],
+				name : 'am',
+				appFolder : 'resource/plugin/extjs/5.10/code/apps',
+				launch : function() {
+					Ext.create('Ext.container.Viewport', {
+								layout : 'fit',
+								items : [{
+											xtype : 'loginlayout'
+										}]
+							});
+				},
+				controllers : [
+						'am.manager.controller.ManagerController',
+						'am.login.controller.LoginController'
+						]
+			});
+		//
+	});
